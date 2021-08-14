1250fb5ecc72337b1fc851246ab3a8fca6cb981e
xland44 <aribarzilai@gmail.com>
2021-08-14 19:41:12 +0300

    Add project files.

---
+++ xland44.github.io/Global.asax.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Web.Http
+
+Public Class Global_asax
+    Inherits HttpApplication
+
+    Sub Application_Start(sender As Object, e As EventArgs)
+        ' Fires when the application is started
+        GlobalConfiguration.Configure(AddressOf WebApiConfig.Register)
+    End Sub
+End Class
